eca0aa3d22b875c6fbcbb82f42d1d67cf1639c4c
digvijay-555 <digvijaydeshmukh5555@outlook.com>
2026-03-04 13:58:21 +0000

    Added database connection files

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose, { MongooseError } from "mongoose";
+import {DB_NAME} from "../constants.js"
+import dotenv from "dotenv";
+
+dotenv.config({
+    path:"./env"
+})
+
+const connectDB = async() => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+}
+
+
+export default connectDB;
