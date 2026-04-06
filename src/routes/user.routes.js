e1f6823a519926dc0f74591015c70975f297b3b6
digvijay-555 <digvijaydeshmukh5555@outlook.com>
2026-04-06 13:11:22 +0000

    Add routes for change password, current user, avatar, cover image, user channel profile, and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -24,4 +24,18 @@ router.route("/register").post(
 
     router.route("/refresh-token").post(refreshAccessToken);
 
+    router.route("/change-password").post(verifyJWT, changePassword);
+
+    router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+    router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar);
+
+    router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+    router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+
+    router.route("/history").get(verifyJWT, getWatchHistory);
+
+
+
 export default router;
